7f9747fdf2b1d5db8dc5938cbf606168001fb23a
Sadhguna Aithal <133648612+ape-with-helmet@users.noreply.github.com>
2023-06-28 07:16:34 +0000

    sjf fcfs idle set

--- second/3inone.c
+++ second/3inone.c
@@ -2,7 +2,7 @@
 void fcfs(int processes[],int n,int burst_time[],int arrival_time[])
 {
     int WT[n],TAT[n],total_WT=0,total_TAT=0,completion_time[n];
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)//sorting in fcfs
     {
         int first=i;
         for (int j = i+1; j < n; j++)
@@ -19,23 +19,29 @@ void fcfs(int processes[],int n,int burst_time[],int arrival_time[])
         temp=processes[i];
         processes[i]=processes[first];
         processes[first]=temp;
+
+        temp=arrival_time[i];
+        arrival_time[i]=arrival_time[first];
+        arrival_time[first]=temp;
     }
-    
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)//calculating times
     {
         if(i==0)
         {
             WT[0]=0;
-            completion_time[0]=burst_time[0];
-            if(arrival_time[0]!=0)
-            {
-                completion_time[0]=burst_time[0]+arrival_time[0];
-            }
+            completion_time[0]=burst_time[0]+arrival_time[0];
             TAT[0]=completion_time[0]-arrival_time[0];
             total_TAT=TAT[0];
             continue;
         }
-        completion_time[i]=completion_time[i-1]+burst_time[i];
+        if(completion_time[i-1]<arrival_time[i])
+        {
+            completion_time[i]=arrival_time[i]+burst_time[i];
+        }
+        else
+        {
+            completion_time[i]=completion_time[i-1]+burst_time[i];
+        }
         TAT[i]=completion_time[i]-arrival_time[i];
         total_TAT+=TAT[i];
         WT[i]=TAT[i]-burst_time[i];
@@ -51,7 +57,7 @@ void fcfs(int processes[],int n,int burst_time[],int arrival_time[])
     printf("AVG turnaround time: %2f\n",(float)total_TAT/n);
     printf("\n");
 }
-void sjf(int processes[],int n,int burst_time[])
+void sjf(int processes[],int n,int burst_time[],int arrival_time[])
 {
     int WT[n],TAT[n],total_WT=0,completion_time[n],total_TAT=0;
     for (int i = 0; i < n; i++)
@@ -71,23 +77,43 @@ void sjf(int processes[],int n,int burst_time[])
         temp=processes[i];
         processes[i]=processes[shortest_job_index];
         processes[shortest_job_index]=temp;
+
+        temp=arrival_time[i];
+        arrival_time[i]=arrival_time[shortest_job_index];
+        arrival_time[shortest_job_index]=temp;
     }
-    WT[0]=0;
-    for (int i = 1; i < n; i++)
+    for (int i = 0; i < n; i++)//calculating times
     {
-        WT[i]=burst_time[i-1]+WT[i-1];
+        if(i==0)
+        {
+            WT[0]=0;
+            completion_time[0]=burst_time[0];
+            if(arrival_time[0]!=0)
+            {
+                completion_time[0]=burst_time[0]+arrival_time[0];
+            }
+            TAT[0]=completion_time[0]-arrival_time[0];
+            total_TAT=TAT[0];
+            continue;
+        }
+        if(completion_time[i-1]<arrival_time[i])
+        {
+            completion_time[i]=arrival_time[i]+burst_time[i];
+        }
+        else
+        {
+            completion_time[i]=completion_time[i-1]+burst_time[i];
+        }
+        TAT[i]=completion_time[i]-arrival_time[i];
+        total_TAT+=TAT[i];
+        WT[i]=TAT[i]-burst_time[i];
         total_WT+=WT[i];
     }
-    for (int i = 0; i < n; i++)
-    {
-        TAT[i]=burst_time[i]+WT[i];
-        total_TAT+=TAT[i];
-    }
     printf("SJF algorithm\n");
-    printf("Processes\tBurst Time\tWaiting TIme\tTurnaround Time\n");
+    printf("Processes\tArrival Time\tBurst Time\tWaiting TIme\tTurnaround Time\tComplete Time\n");
     for (int i = 0; i < n; i++)
     {
-        printf("%d\t\t%d\t\t%d\t\t%d\n",processes[i],burst_time[i],WT[i],TAT[i]);
+        printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n",processes[i],arrival_time[i],burst_time[i],WT[i],TAT[i],completion_time[i]);
     }
     printf("AVG waiting time: %2f\n",(float)total_WT/n);
     printf("AVG turnaround time: %2f\n",(float)total_TAT/n);
@@ -169,5 +195,5 @@ void main()
     //scanf("%d",&quantum);
     fcfs(processes,n,burst_time,arrival_time);
     //roundRobin(processes,n,burst_time,quantum);
-    //sjf(processes,n,burst_time);
+    sjf(processes,n,burst_time,arrival_time);
 }
